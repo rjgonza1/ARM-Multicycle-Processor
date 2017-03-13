74a95d8ab23830d97382cc9f267512eadf305f64
Marcos A. Avila <marcosa@uci.edu>
2017-03-12 21:23:28 -0700

    IFetch edit

--- design/IFetch.sv
+++ design/IFetch.sv
@@ -1,11 +1,11 @@
 module IFetch(
-	input logic clk,
+	input logic clk, reset, stall,
 	input logic [31:0] PC,
 	output logic [31:0] InstrF, PCPlus4F);
 	
 	logic [31:0] PCF;
 	
-	flopr #(32) IFReg(clk, 1'b0, PC, PCF);
+	flopr #(32) IFReg((clk & ~stall), reset, PC, PCF);
 
 	imem imem(PCF, InstrFetch);
 
