a7195194f73b92b1988b5ae385060091bf117169
Marcos A. Avila <marcosa@uci.edu>
2017-03-13 14:59:03 -0700

    Updated IFetch.sv, imem will provide InstrF from top.

--- design/IFetch.sv
+++ design/IFetch.sv
@@ -1,9 +1,7 @@
 module IFetch(
 	input logic clk, reset, StallF,
 	input logic [31:0] PC,	// Next PC value
-	output logic [31:0] PCF,
-	input logic [31:0] Instr,
-	output logic [31:0] InstrF, PCPlus4F);
+	output logic [31:0] PCF, PCPlus4F);
 
 	flopr #(32) IFReg((clk & ~stall), reset, PC, PCF);
 
