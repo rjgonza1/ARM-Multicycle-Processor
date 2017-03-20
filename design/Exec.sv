0c85b33ccf2630662d7fba8cca7a68833ee85975
julian marshall andrews <julianma@zuma.eecs.uci.edu>
2017-03-19 23:55:33 -0700

    continued development of Exec.sv and commented some Flag ports in condlogic.sv that shouldn't be necessary

--- design/Exec.sv
+++ design/Exec.sv
@@ -1,30 +1,34 @@
 module Exec(
+	input logic clk, reset,
 	input logic PCSrcD,
 	input logic RegWriteD,
 	input logic MemtoRegD,
 	input logic MemWriteD,
-	input logic ALUControlD,
+	input logic [3:0] ALUControlD,
+	input logic BranchD,
 	input logic ALUSrcD,
-	input logic FlagWriteD,
+	input logic [1:0] FlagWriteD,
 	input logic ImmSrcD,
+	input logic [3:0] CondD,
 	input logic [3:0] WriteAddrD,
 	input logic [31:0] Rd1D, Rd2D, ExtD,
 	input logic [1:0] forwardAE, forwardBE,
 	input logic [31:0] ALUResultM,
 	input logic [31:0] ResultW,
-	output logic PCSrcE,
-	output logic RegWriteE,
-	output logic MemtoRegE,
-	output logic MemWriteE,
-	output logic [31:0] ALUResultE,
-	output logic [31:0] WriteDataE,
-	output logic [3:0] WriteAddrE
+	output logic PCSrcM,
+	output logic RegWriteM,
+	output logic MemtoRegM,
+	output logic MemWriteM,
+	output logic [31:0] ALUResultM,
+	output logic [31:0] WriteDataM,
+	output logic [3:0] WriteAddrM
 	);
 
 	// internal signal declarations
-		logic 	PCSrcE, RegWriteE, MemtoRegE, MemWriteE, 
-			ALUControlE, ALUSrcE, FlagWriteE, ImmSrcE;
-		logic [3:0] WriteAddrE;
+		logic 	PCSrcE, RegWriteE, MemWriteE, MemtoRegE, 
+			ALUSrcE, FlagWriteE, ImmSrcE, BranchE;
+		logic [1:0] FlagWriteE;
+		logic [3:0] WriteAddrE, CondE, ALUControlE;
 		logic [31:0] Rd1E, Rd2E, ExtE;
 
 		logic [31:0] OpA, OpB, nonImmOpB;
@@ -33,13 +37,20 @@ module Exec(
 
 	// Assignments and logic
 	
-		MemtoRegM <= MemtoRegD;
-		WriteAddrM <= WriteAddrD;
+		MemtoRegM <= MemtoRegE;
+		WriteAddrM <= WriteAddrE;
 		WriteDataM <= nonImmOpB;
 
 	// declaring other modules
 
-		flopr EReg #(/*TODO find out actual size of this flop*/);
+		pipereg reg (clk, reset, PCSrcD, RegWriteD, MemtoRegD, MemWriteD, ALUSrcD, 
+				FlagWriteD, ALUControlD, );
+		
+		// INPUT clk, reset, [3:0] cond, [3:0] ALUFlags
+		// INPUT [1:0] FlagW, PCS, RegW, MemW,
+		// OUTPUT PCSrc, RegWrite, MemWrite
+		condlogic cond (clk, reset, condE, ALUFlags, FlagWriteE, PCSrcE, RegWriteE, MemWriteE, BranchE,
+				PCSrcM, RegWriteM, MemWriteM); 
 
 		mux3 m1(Rd1E, ResultW, ALUResultM, forwardAE, OpA);
 		mux3 m2(Rd2E, ResultW, ALUResultM, forwardBE, nonImmOpB);
--- design/condlogic.sv
+++ design/condlogic.sv
@@ -1,14 +1,17 @@
 module condlogic(input logic clk, reset,
 		input logic [3:0] Cond,
 		input logic [3:0] ALUFlags,
-		output logic [3:0] StatusRegister, Flags, //made Flags an output
+		//input logic [3:0] PrevFlags, // added PrevFlags, Flags output loops back through pipereg back to here
+		//output logic [3:0]  Flags, // removed StatusRegister (add here if we actually need) -Julian
 		input logic [1:0] FlagW,
-		input logic PCS, RegW, MemW,
+		input logic PCS, RegW, MemW, Branch,
 		output logic PCSrc, RegWrite, MemWrite
 		);
 
 	logic [1:0] FlagWrite;
 	logic CondEx;
+ 	// used to store ALUFlags until the next clock cycle so the next pipeline instruction can use them
+	logic [3:0] Flags;
 
 	flopenr #(2) flagreg1(	clk, reset, FlagWrite[1], 
 				ALUFlags[3:2], Flags[3:2]);
@@ -21,7 +24,7 @@ module condlogic(input logic clk, reset,
 	assign FlagWrite = FlagW & {2{CondEx}};
 	assign RegWrite = RegW & CondEx;
 	assign MemWrite = MemW & CondEx;
-	assign PCSrc = PCS & CondEx;
+	assign PCSrc = (PCS | Branch) & CondEx; // changed this expression to include Branch -Julian
 
 endmodule
 
