8ef84f136c6af189747e5c43a39c42caa0725ad8
julian marshall andrews <julianma@zuma.eecs.uci.edu>
2017-03-19 20:50:05 -0700

    created a mux3.sv to be used for Exec stage

---
+++ design/mux3.sv
@@ -0,0 +1,32 @@
+module mux3
+	#(parameter WIDTH = 8)
+	(
+		input logic [WIDTH-1 : 0] d0, d1, d2,
+		input logic [1:0] s,
+		output logic [WIDTH-1 : 0] y
+	);
+
+	case(s)
+
+		2'b00:
+		begin
+			y = d0;
+		end
+
+		2'b01:
+		begin
+			y = d1;
+		end
+
+		2'b10:
+		begin
+			y = d2;
+		end
+
+		default:
+		begin
+			y = WIDTH'bx;
+		end
+	endcase
+
+endmodule
