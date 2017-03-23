2420fb78f78cffcf1e003e419b34481b44196898
Marcos A. Avila <marcosa@uci.edu>
2017-03-23 16:57:00 -0700

    Added pipereg.sv

---
+++ design/pipereg.sv
@@ -0,0 +1,31 @@
+module pipereg(
+    input logic clk, reset, PCSrcD, RegWriteD, MemtoRegD, MemWriteD, ALUSrcD, 
+    input logic [1:0] FlagWriteD,
+    input logic [3:0] ALUControlD, Flags, CondD, RdD,
+    output logic PCSrcE, RegWriteE, MemtoRegE, MemWriteE, ALUSrcE,
+    output logic [1:0] FlagWriteE,
+    output logic [3:0] ALUControlE, CondE, FlagsE, RdE
+    );
+
+    always_ff @(posedge clk, posedge reset)
+         if (reset)
+            PCSrcE <= 0;
+            RegWriteE <= 0;
+            MemtoRegE <= 0;
+            MemWriteE <= 0;
+            ALUSrcE <= 0;
+            FlagWriteE <= 2'b00;
+            ALUControlE <= 4'b0000;
+            CondE <= 4'b0000;
+            FlagsE <= 4'b0000;
+        else
+            PCSrceE <= PCSrcD;
+            RegWriteE <= RegWriteD;
+            MemtoRegE <= MemtoRegD;
+            MemWriteE <= MemWriteD;
+            ALUSrcE <= ALUSrcD;
+            FlagWriteE <= FlagWriteD;
+            ALUControlE <= ALUControlD;
+            CondE <= CondD;
+            FlagsE <= Flags;
+endmodule
