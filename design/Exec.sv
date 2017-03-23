e10b422b59091a1f1555dfe7655ee23eb87d1a5c
Julian Andrews <Julian Andrews>
2017-03-22 18:22:25 -0700

    I updated Exec.sv adding the shifter and input ports necessary for i

--- design/Exec.sv
+++ design/Exec.sv
@@ -1,5 +1,5 @@
 module Exec(
-	input logic clk, reset,
+	input logic clk, reset, stall, flush
 	input logic PCSrcD,
 	input logic RegWriteD,
 	input logic MemtoRegD,
@@ -15,6 +15,13 @@ module Exec(
 	input logic [1:0] forwardAE, forwardBE,
 	input logic [31:0] ALUResultM,
 	input logic [31:0] ResultW,
+	// shifter input logic
+	input logic Immediate,
+	input logic [1:0] Sh,
+	input logic [4:0] Shamt,
+	input logic IsRegister, Carry,
+	input logic [31:0] RsShiftD, // remember to parse this to 8 bits
+	
 	output logic PCSrcM,
 	output logic RegWriteM,
 	output logic MemtoRegM,
@@ -34,6 +41,9 @@ module Exec(
 		logic [31:0] OpA, OpB, nonImmOpB;
 		logic [3:0] ALUFlags;
 		logic carryIn;
+		// signal for shifter output
+		logic ShiftOut;
+		logic ShiftCarry;
 
 	// Assignments and logic
 	
@@ -42,19 +52,31 @@ module Exec(
 		WriteDataM <= nonImmOpB;
 
 	// declaring other modules
-
-		pipereg reg (clk, reset, PCSrcD, RegWriteD, MemtoRegD, MemWriteD, ALUSrcD, 
-				FlagWriteD, ALUControlD, );
+		// I did not include Flags in Exec.sv because the flags
+		// already get delayed by 1 clock cycle in the condlogic.sv.
+		// If this is not correct, add flags as an input port and
+		// include flags into this pipereg below. -Julian
+		pipereg reg ((clk & ~stall), flush, PCSrcD, RegWriteD, MemtoRegD, MemWriteD, ALUSrcD, 
+				FlagWriteD, ALUControlD, /*Flags*/, CondD, Rd1D, Rd2D, RsShiftD, ExtD,
+				PCSrcE, RegWriteE, MemWriteE, ALUSrcE, FlagWriteE, ALUControlE, CondE, 
+				FlagsE, Rd1E, Rd2E, RsShiftE, ExtE);
 		
 		// INPUT clk, reset, [3:0] cond, [3:0] ALUFlags
 		// INPUT [1:0] FlagW, PCS, RegW, MemW,
 		// OUTPUT PCSrc, RegWrite, MemWrite
 		condlogic cond (clk, reset, condE, ALUFlags, FlagWriteE, PCSrcE, RegWriteE, MemWriteE, BranchE,
 				PCSrcM, RegWriteM, MemWriteM); 
-
+		
+		// INPUT [31:0] Rm, [7:0] RsShift, Immediate, [1:0] Sh, [4:0] Shamt, IsRegister, Carry
+		// OUTPUT [31:0] Result, ShiftCarry,
+		// INPUT [3:0] ALUControl
+		shifter shft (nonImmOpB, RsShift, Immediate, Sh, Shamt, IsRegister, Carry, 
+				ShiftOut, ShiftCarry, ALUControlE);
+		
+		// mux
 		mux3 m1(Rd1E, ResultW, ALUResultM, forwardAE, OpA);
 		mux3 m2(Rd2E, ResultW, ALUResultM, forwardBE, nonImmOpB);
-		mux2 m3(nonImmOpB, ExtE, ALUSrcE, OpB);
+		mux2 m3(ShiftOut, ExtE, ALUSrcE, OpB);
 
 		alu a(OpA, OpB, ALUControlE, ALUResultE, ALUFlags, carryIn);
 
