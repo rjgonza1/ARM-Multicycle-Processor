5b35106726c4830c44cb5625f79451d2812a1b3f
rjgonza1 <rjgonza1@uci.edu>
2017-03-23 21:02:17 -0700

    Update wbPipereg.sv

--- design/wbPipereg.sv
+++ design/wbPipereg.sv
@@ -8,18 +8,24 @@ module wbPipereg(
     );
 
     always_ff @(posedge clk, posedge reset)
+    begin
          if (reset)
+            begin
             PCSrcW <= 0;
             RegWriteW <= 0;
             MemtoRegW <= 0;
             RdW <= 4'b0000;
             ALUResultW <= 32'b0;
             ReadDataW <= 32'b0;
+            end
         else
+            begin
             PCSrcW <= PCSrcM;
             RegWriteW <= RegWritMe;
             MemtoRegW <= MemtoRegM;
             RdW <= RdM;
             ALUResultW <= ALUResultM;
             ReadDataW <= ReadDataM;
+            end
+    end 
 endmodule
