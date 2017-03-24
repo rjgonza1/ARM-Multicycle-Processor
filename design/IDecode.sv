b5479fb7b45e4e36609db3af84a7642fbcbf39e0
rjgonza1 <rjgonza1@uci.edu>
2017-03-23 19:49:03 -0700

    Update IDecode.sv
    added output for RA

--- design/IDecode.sv
+++ design/IDecode.sv
@@ -3,10 +3,11 @@ module IDecode(
 	input logic [31:0] InstrF, PCPlus8, ResultW,
      output logic MemWriteD, MemtoRegD, PCSrcD, ALUSrcD, RegWriteD,   
 	output logic [1:0] FlagWriteD, 
-	output logic [3:0] byteEnable, ALUControlD, RdD, CondD,   
+	output logic [3:0] byteEnable, ALUControlD, RdD, CondD, RA1D, RA2D,   
 	output logic [31:0] SrcAD, ShiftSourceD, ExtImmD, Rs
 	);
-
+	
+	logic [31:0] 
 	logic [3:0] RA1, RA2;
 	logic [1:0] RegSrcD, ImmSrcD;
 	logic branch_link;
@@ -20,8 +21,8 @@ module IDecode(
 			ImmSrcD, RegSrcD, ALUControlD, byteEnable, branch_link);
 
 // register file logic 
-	mux2 #(4) ra1mux(InstrD[19:16], 4'b1111, RegSrcD[0], RA1); 
-	mux2 #(4) ra2mux(InstrD[3:0], InstrD[15:12], RegSrcD[1], RA2); 
+	mux2 #(4) ra1mux(RA1D, 4'b1111, RegSrcD[0], RA1); 
+	mux2 #(4) ra2mux(RA2D, InstrD[15:12], RegSrcD[1], RA2); 
 
 	regfile rf(clk, RegWriteW, RA1, RA2, InstrD[11:8], InstrD[15:12], ResultW, 
 			PCPlus8, SrcAD, ShiftSourceD, Rs, branch_link);
