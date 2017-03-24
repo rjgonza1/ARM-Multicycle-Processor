e36f758daf5f955fb473b08f886e1637ca4c1ccc
DaiconV <marcosa@uci.edu>
2017-03-23 18:36:05 -0700

    Update arm.sv

--- design/arm.sv
+++ design/arm.sv
@@ -7,22 +7,23 @@ module arm(	input logic clk, reset,
 		input logic [31:0] ReadData
 	);
 
-	logic [3:0] ALUFlags;
-	logic [3:0] StatusRegister;
-	logic RegWrite, ALUSrc, MemtoReg, PCSrc;
-	logic [1:0] RegSrc, ImmSrc;
-  logic [3:0] ALUControl;
-	logic branch_link;
-
+// 	logic [3:0] ALUFlags;
+// 	logic [3:0] StatusRegister;
+// 	logic RegWrite, ALUSrc, MemtoReg, PCSrc;
+// 	logic [1:0] RegSrc, ImmSrc;
+// 	logic [3:0] ALUControl;
+// 	logic branch_link;
 	// add byteEnable as output of controller later
 
-	controller c(clk, reset, Instr[31:12], Instr[11:0], ALUFlags, StatusRegister,
-			RegSrc, RegWrite, ImmSrc, ALUSrc, ALUControl,
-			MemWrite, MemtoReg, PCSrc, byteEnable, branch_link);
+// 	controller c(clk, reset, Instr[31:12], Instr[11:0], ALUFlags, StatusRegister,
+// 			RegSrc, RegWrite, ImmSrc, ALUSrc, ALUControl,
+// 			MemWrite, MemtoReg, PCSrc, byteEnable, branch_link);
 
-	datapath dp(clk, reset, 
-			RegSrc, RegWrite, ImmSrc, ALUSrc, ALUControl,
-			MemtoReg, PCSrc, ALUFlags, StatusRegister, PC, Instr, ALUResult,
-			WriteData, ReadData, branch_link);
+// 	datapath dp(clk, reset, 
+// 			RegSrc, RegWrite, ImmSrc, ALUSrc, ALUControl,
+// 			MemtoReg, PCSrc, ALUFlags, StatusRegister, PC, Instr, ALUResult,
+// 			WriteData, ReadData, branch_link);
 
+	pipeDatapath dp(clk, reset, MemWrite, byteEnable, Instr, ReadData, PC, ALUResult, WriteData);
+	
 endmodule
