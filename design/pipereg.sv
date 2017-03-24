731dd270d0e5acd54b72df5cb90adb8b18f1bf97
jmandrews0 <jmandrews000@gmail.com>
2017-03-23 17:12:39 -0700

    Update pipereg.sv

--- design/pipereg.sv
+++ design/pipereg.sv
@@ -1,10 +1,10 @@
 module pipereg(
     input logic clk, reset, PCSrcD, RegWriteD, MemtoRegD, MemWriteD, ALUSrcD, 
     input logic [1:0] FlagWriteD,
-    input logic [3:0] ALUControlD, CondD, RdD, // removed Flags from here
+    input logic [3:0] ALUControlD, CondD, Rd1D, Rd2D, RsShiftD, ExtD, // removed Flags from here
     output logic PCSrcE, RegWriteE, MemtoRegE, MemWriteE, ALUSrcE,
     output logic [1:0] FlagWriteE,
-    output logic [3:0] ALUControlE, CondE, RdE // removed FlagsE from here
+    output logic [3:0] ALUControlE, CondE, Rd1E, Rd2E, RsShiftE, ExtE // removed FlagsE from here
     );
 
     always_ff @(posedge clk, posedge reset)
@@ -17,7 +17,10 @@ module pipereg(
             FlagWriteE <= 2'b00;
             ALUControlE <= 4'b0000;
             CondE <= 4'b0000;
-            //FlagsE <= 4'b0000;
+            Rd1E <= 3'b000;
+            Rd2D <= 3'b000;
+            RsShiftD <= 3'b000;
+            ExtD <= 3'b000;
         else
             PCSrceE <= PCSrcD;
             RegWriteE <= RegWriteD;
@@ -27,5 +30,8 @@ module pipereg(
             FlagWriteE <= FlagWriteD;
             ALUControlE <= ALUControlD;
             CondE <= CondD;
-            //FlagsE <= Flags;
+            Rd1E <= Rd1D;
+            Rd2E <= Rd2D;
+            RsShiftE <= RsShiftD;
+            ExtE <= ExtD;
 endmodule
