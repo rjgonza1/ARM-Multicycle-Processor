c5b1e39f38d380b88fbf3f7bcc8382fb181ec33b
rjgonza1 <rjgonza1@uci.edu>
2017-03-23 22:43:20 -0700

    Update pipedDatapath.sv

--- design/pipedDatapath.sv
+++ design/pipedDatapath.sv
@@ -34,8 +34,8 @@ module pipedDatapath(
     
 	// Execute
 	Exec exec(clk, (reset || FlushE), PCSrcD, RegWriteD, MemtoRegD, MemWriteD, ALUControlD, BranchD, ALUSrcD, FlagWriteD,
-		  CondD, RdD, RA1, RA2, SrcAD, ShiftSourceD, ExtImmD, forwardAE, forwardBE, ResultW, ALUResultM, Rs, Instruction[25],
-		  Instruction[6:5], Instruction[11:7], Instruction[4], PCSrcE, RegWriteE,
+		  CondD, RdD, RA1, RA2, SrcAD, ShiftSourceD, ExtImmD, forwardAE, forwardBE, ResultW, ALUResultM, Rs, InstrF[25],
+		  InstrF[6:5], InstrF[11:7], InstrF[4], PCSrcE, RegWriteE,
 		  MemtoRegE, MemWriteE, BranchTakenE, RdE, RA1E, RA2E, ALUResultE, WriteDataE);
 
 	// Memory
